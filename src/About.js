036be943da49718180be47fa9b43c14533e9a9b1
GitHubXiaoZ <48108389+GitHubXiaoZ@users.noreply.github.com>
2019-03-13 02:50:47 -0400

    Add bar displayables to about.js

--- src/About.js
+++ src/About.js
@@ -1,14 +1,6 @@
 import React, { Component } from "react";
 
 class About extends Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-        percentage: 50
-    }
-  }
-
   render() {
     return (
       <div>
@@ -17,13 +9,25 @@ class About extends Component {
         <p>My Skillset: </p>
         <div>
           <ul class="skills">
+          <ExperienceBar percentage = {skillset.HTML} />
           <li>HTML</li> 
-          <ExperienceBar percentage = {this.state.percentage} />
+            
+          <ExperienceBar percentage = {skillset.CSS} />
           <li>CSS</li>
+          
+          <ExperienceBar percentage = {skillset.Javascript} />
           <li>Javascript</li>
+          
+          <ExperienceBar percentage = {skillset.ReactJS} />
           <li>ReactJS</li>
+
+          <ExperienceBar percentage = {skillset.Python} />
           <li>Python</li>
+
+          <ExperienceBar percentage = {skillset.Java} />
           <li>Java</li>
+
+          <ExperienceBar percentage = {skillset.CSharp} />
           <li>C#</li>
           </ul>
         </div>
@@ -32,17 +36,31 @@ class About extends Component {
   }
 }
 
+const skillset = {
+  HTML: 45,
+  CSS: 50,
+  Javascript: 60,
+  ReactJS: 55,
+  Python: 75,
+  Java: 25,
+  CSharp: 30
+}
+
+
 function ExperienceBar(props) {
     return (
         <div className="experience-bar">
         <Progress percentage = {props.percentage} />
+        <div className="text"> {props.text} </div>
         </div>
 
     )
 }
 
 function Progress(props) {
-return <div className="progress" style={{ width: `${props.percentage}%`}} />
+    return (
+        <div className="progress" style={{ width: `${props.percentage}%`}} />
+    )
 }
- 
+
 export default About;
