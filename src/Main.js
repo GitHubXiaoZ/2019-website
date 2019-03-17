37a8702149897ad8ff9694256bd94f5b2b0a3916
GitHubXiaoZ <48108389+GitHubXiaoZ@users.noreply.github.com>
2019-03-17 01:51:59 -0400

    Update main.js to include dropdown menu function for mobile interface

--- src/Main.js
+++ src/Main.js
@@ -10,6 +10,32 @@ import Projects from "./Projects";
 import Contact from "./Contact";
 
 class Main extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      displayMenu: false
+    }
+
+    this.displayMenu = this.displayMenu.bind(this)
+    this.hideMenu = this.hideMenu.bind(this)
+  }
+  
+  displayMenu(event) {
+    event.preventDefault()
+
+    this.setState({ displayMenu: true }, () => {
+      document.addEventListener("click", this.hideMenu)
+      })
+    }
+  
+    hideMenu() {
+      this.setState({ displayMenu: false }, () => {
+        document.removeEventListener("click", this.hideMenu)
+      })
+  
+  }
+
   render() {
     return (
       <HashRouter>
@@ -21,6 +47,17 @@ class Main extends Component {
             <li><NavLink to="/contact">Contact</NavLink></li>
           </ul>
           <div className="content">
+          <span className="buttonMenu" onClick = {this.displayMenu}>Menu</span>
+              {
+                  this.state.displayMenu ? (
+                    <ul className="menu">
+                      <li><NavLink exact to="/">Home</NavLink></li>
+                      <li><NavLink to="/about">About</NavLink></li>
+                      <li><NavLink to="/projects">Projects</NavLink></li>
+                      <li><NavLink to="/contact">Contact</NavLink></li>
+                    </ul>
+                  ) : (null)
+              }
             <Route exact path="/" component={Home}/>
             <Route path="/about" component={About}/>
             <Route path="/projects" component={Projects}/>
@@ -28,7 +65,8 @@ class Main extends Component {
           </div>
         </div>
        </HashRouter>  
-    );
+       
+    )
   }
 }
  
